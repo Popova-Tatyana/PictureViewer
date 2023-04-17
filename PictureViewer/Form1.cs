fa7da6e89db62b03e55701da29bc15f93a6e4842
207dc <207dc@ROCKET>
2023-04-17 12:05:32 +0300

    Добавьте файлы проекта.

---
+++ PictureViewer/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            // Показать окно "Открыть файл". Если пользователь нажмет кнопку ОК,
+            // загрузить изображение, которое выбрал пользователь.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Очистить изображение.
+            pictureBox1.Image = null;
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+            // Показать окно "Цвет". Если пользователь нажмет кнопку ОК,
+            // изменить фон PictureBox на выбранный пользователем цвет.
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            // Закрыть форму.
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // Если пользователь установит флажок "Растягивать",
+            // изменить свойство PictureBox's
+            // SizeMode на "StretchImage". Если пользователь снимет
+            // флажок, изменить его свойство на "Normal".
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
+    }
+}
